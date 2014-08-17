22446cf296bf6da0633c072da6963d7bf5287a67
Enrico Reinsdorf <enrico.reinsdorf@re-design.de>
2014-08-17 12:05:57 +0200

    Gruntfile überarbeitet

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,55 +1,32 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    concat: {
-      options: {
-        separator: ';'
-      },
-      dist: {
-        src: ['src/**/*.js'],
-        dest: 'dist/<%= pkg.name %>.js'
-      }
-    },
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
-      },
-      dist: {
-        files: {
-          'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
-        }
-      }
-    },
-    qunit: {
-      files: ['test/**/*.html']
-    },
-    jshint: {
-      files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
-      options: {
-        // options here to override JSHint defaults
-        globals: {
-          jQuery: true,
-          console: true,
-          module: true,
-          document: true
-        }
-      }
-    },
-    watch: {
-      files: ['<%= jshint.files %>'],
-      tasks: ['jshint', 'qunit']
-    }
-  });
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
 
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-qunit');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-concat');
+		meta: {
+			banner: '/* this ist the minified js, build in <%= grunt.template.today(); %>.*/'
+		},
 
-  grunt.registerTask('test', ['jshint', 'qunit']);
+		min: {
+			dist: {
+				src: ['<banner>', 'www/js/*.js'],
+				dest: 'main.min.js'
+			}
+		},
 
-  grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify']);
+		watch: {
+			files: ['www/js/*.js'],
+			tasks: ['min:dist']
+		}
+	});
 
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-qunit');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+
+	grunt.registerTask('default', [
+		'min:dist'
+	]);
 };
