0cfb83d278838bc2afd41de7a264dd102f8d076f
Enrico Reinsdorf <enrico.reinsdorf@re-design.de>
2014-08-17 13:22:22 +0200

    Einfache Formular-Version
    * Formular angepasst
    * CSS-Styles erstellt
    * JavaScript verschickt per Post-AJAX die Daten
    * Jquery blendet das Ergebnis ein

--- www/js/main.js
+++ www/js/main.js
@@ -4,24 +4,52 @@
 
 (function($){
 	'use strict';
-	$(document).ready(function(){
+	$(document).ready(function() {
+
+		$('#contact-form .form-group.required > label').append($('<span>').html('*'));
 
 		$('#contact-form').on('submit', function(event) {
 			event.preventDefault();
+			var proceed = true;
 
-			$.post('mail.php',
-				{
+			if(proceed) //everything looks good! proceed...
+			{
+				$.post('mail/index.php',
+					{
+						'user_email': 'postmaster@localhost',
+						'user_name': 'Postmaster',
+						'from_email': 'enrico.reinsdorf@re-design.de',
+						'from_name': 'Enrico Reinsdorf',
+						'subject': 'Test Subject',
+						'msg': ''
+					})// $.post(
 
-				})
-				.done(function(data) {
-					console.log('success', data);
-				})
-				.fail(function(data) {
-					console.log('fail');
-				})
-				.always(function(data) {
-					console.log('finished');
-				});
-		});
-	});
+					.done(function(data) {
+						console.log('success', data);
+						$("#contact-results").slideDown().html('');
+					})// $.post().done()
+
+					.fail(function(data) {
+						console.log('fail', data.responseText);
+
+						$('#contact-results')
+							.hide()
+							.html('Ein Fehler ist aufgetreten.')
+							.slideDown();
+					})// $.post().done()
+
+					.always(function(data) {
+						console.log('finished');
+
+						$('#contact-results')
+							.hide()
+							.html('Email verschickt')
+							.slideDown();
+					}); // $.post.done()
+			}
+			$("#contact-results")
+				.hide()
+				.html('');
+		}); // contact-form.on()
+	}); // document.ready()
 })(jQuery);
