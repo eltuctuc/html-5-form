04bd9f555331900b1a4c121951a1cc28306205df
Enrico Reinsdorf <enrico.reinsdorf@re-design.de>
2014-09-29 18:52:34 +0200

    Captcha-Klasse eingebaut

---
+++ www/mail/captcha.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Project: Weblabor Html5 Formular
+ * User: Enrico <$EMAIL>
+ * Date: 29.09.2014
+ */
+include('../vendor/gregwar/captcha/Gregwar/Captcha/CaptchaBuilderInterface.php');
+include('../vendor/gregwar/captcha/Gregwar/Captcha/PhraseBuilderInterface.php');
+include('../vendor/gregwar/captcha/Gregwar/Captcha/CaptchaBuilder.php');
+include('../vendor/gregwar/captcha/Gregwar/Captcha/PhraseBuilder.php');
+
+use Gregwar\Captcha\CaptchaBuilder;
+session_start();
+
+header('Content-type: image/jpeg');
+
+$builder = CaptchaBuilder::create($_SESSION['phrase']);
+$builder->build();
+
+// Example: storing the phrase in the session to test for the user
+// input later
+$_SESSION['phrase'] = $builder->getPhrase();
+
+$builder->output();
